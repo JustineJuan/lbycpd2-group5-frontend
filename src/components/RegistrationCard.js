9abd436dcbacc59d6199d71373d65da4f27257de
Rafael Gabriel Arceo <rafael_gabriel_arceo@dlsu.edu.ph>
2021-05-19 15:11:22 +0800

    Login page now communicates to API (Chrome only)

--- src/components/RegistrationCard.js
+++ src/components/RegistrationCard.js
@@ -1,21 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { Card, Row, Col, Form, Button, Alert } from "react-bootstrap";
+import { useHistory } from "react-router-dom";
+import axiosConfig from './../axiosConfig'
 
 const valid = (string1, string2) => {
   return string1 == string2;
 };
 
 // calls the TodoEXP API and registers the user. Catches any error and returns if the registration was successful.
-const apiCalls = () => {
-    
-}
 
 const RegistrationCard = () => {
+  let history = useHistory();
+  const createUserObject = () => {
+    const userObject = {
+      firstName: firstName,
+      lastName: lastName,
+      email: emailCheck0,
+      password: passwordInput0,
+      authorities: "USER",
+    };
+    return userObject;
+  };
+
+  const apiCalls = (e) => {
+    e.preventDefault();
+    axiosConfig.post('/register', createUserObject()).then((res) => {console.log(res)}).catch((err) => {console.log(err)})
+    history.push("/register/reqsent");
+  };
+
   const [passwordInput0, setPasswordInput0] = useState("");
   const [passwordInput1, setPasswordInput1] = useState("");
   const [emailCheck0, setEmailCheck0] = useState("");
   const [emailCheck1, setEmailCheck1] = useState("");
   const [alertStatus, setAlertStatus] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastname] = useState("");
 
   useEffect(() => {
     console.log("Checking");
@@ -26,7 +45,8 @@ const RegistrationCard = () => {
     } else {
       setAlertStatus(false);
     }
-  }, [passwordInput1]);
+  }, [passwordInput0, passwordInput1, emailCheck0, emailCheck1]);
+
   return (
     <div>
       <Card>
@@ -35,7 +55,7 @@ const RegistrationCard = () => {
         </Card.Title>
         <Card.Body>
           <p>Let's get you started. We just need some information from you!</p>
-          <Form>
+          <Form onSubmit={apiCalls}>
             <Row>
               <Col>
                 <Form.Group controlId="formBasicFirstName">
@@ -43,6 +63,10 @@ const RegistrationCard = () => {
                   <Form.Control
                     type="text"
                     placeholder="Enter your first name here."
+                    defaultValue={firstName}
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
                   />
                 </Form.Group>
               </Col>
@@ -52,6 +76,10 @@ const RegistrationCard = () => {
                   <Form.Control
                     type="text"
                     placeholder="Enter your last name here."
+                    defaultValue={lastName}
+                    onChange={(e) => {
+                      setLastname(e.target.value);
+                    }}
                   />
                 </Form.Group>
               </Col>
@@ -81,7 +109,6 @@ const RegistrationCard = () => {
             </Form.Group>
             <Row>
               <Col>
-                {" "}
                 <Form.Group controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
                   <Form.Control
@@ -110,7 +137,7 @@ const RegistrationCard = () => {
             </Row>
             <Row>
               <Col>
-                <Button variant="primary" type="submit" disabled={!alertStatus}>
+                <Button variant="primary" disabled={!alertStatus} type="submit">
                   Register
                 </Button>
               </Col>
@@ -119,7 +146,7 @@ const RegistrationCard = () => {
         </Card.Body>
       </Card>
       <Col>
-        <Alert variant="danger" show={!alertStatus}>
+        <Alert variant="danger" show={!alertStatus} className="mt-4">
           <Alert.Heading>Email or password not equal!</Alert.Heading>
           <p>
             Most probably you didn't input them correctly. Please try again.
---
+++ src/webpages/registrationLinkSent.js
@@ -0,0 +1,9 @@
+const RegistrationLinkSent = () => {
+    return (
+        <div>
+            <p>Confirmation sent to your email! If you did not recieve one, please try registering again. </p>
+        </div>
+    );
+}
+
+export default RegistrationLinkSent;
